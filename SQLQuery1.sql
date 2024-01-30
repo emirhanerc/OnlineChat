f1a9eb72a01f4e4981d0116fd8410a40705fed52
emirhanerc <150293724+emirhanerc@users.noreply.github.com>
2024-01-30 14:33:37 +0300

    SQLQuery1.sql

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,26 +1,26 @@
-﻿CREATE TABLE [dbo].[Chats] (
-    [Id]           INT             IDENTITY (1, 1) NOT NULL,
-    [UserOneId]     NVARCHAR (20)  NOT NULL,
-    [UserTwoId]     NVARCHAR (20)  NOT NULL,
-    [LastMessageAt] DATETIME2 DEFAULT GETDATE()  NOT NULL,
-    CONSTRAINT [PK_Chats] PRIMARY KEY CLUSTERED ([Id] ASC)
+CREATE TABLE [dbo].[Chats](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[UserIDs] [nchar](255) NOT NULL,
+	[Name] [nchar](255) NULL,
+	[LastMessageAt] [datetime2](7) NOT NULL
 );
 
-CREATE TABLE [dbo].[GroupChats] (
-    [Id]           INT             IDENTITY (1, 1) NOT NULL,
-    [UserIDs]     NVARCHAR (20)  NOT NULL,
-    [Name]     NVARCHAR (20)  NOT NULL,
-    [LastMessageAt] DATETIME2 DEFAULT GETDATE()  NOT NULL,
-    CONSTRAINT [PK_GroupChats] PRIMARY KEY CLUSTERED ([Id] ASC)
+CREATE TABLE [dbo].[Users](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Username] [nvarchar](30) NOT NULL,
+	[Token] [nvarchar](30) NOT NULL,
+	[Password] [nvarchar](30) NOT NULL,
+	[IsAdmin] [int] NOT NULL,
+	[LastOnlineAt] [datetime2](7) NOT NULL,
+	[ProfileImage] [varbinary](max) NULL,
+	[PictureID] [varchar](10) NOT NULL,
+ CONSTRAINT [PK_Users] PRIMARY KEY CLUSTERED 
 );
-
-CREATE TABLE [dbo].[Users] (
-    [Id]           INT             IDENTITY (1, 1) NOT NULL,
-    [Username]     NVARCHAR (30)  NOT NULL,
-    [Token]        NVARCHAR (30)  NOT NULL,
-    [Password]     NVARCHAR (30)  NOT NULL,
-    [IsAdmin]      INT             NOT NULL,
-    [LastOnlineAt] DATETIME2 DEFAULT GETDATE()  NOT NULL,
-    [ProfileImage] VARBINARY (MAX) DEFAULT (0x) NOT NULL,
-    CONSTRAINT [PK_Users] PRIMARY KEY CLUSTERED ([Id] ASC)
+CREATE TABLE [dbo].[Messages](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Message] [varchar](255) NOT NULL,
+	[CreatedAt] [datetime2](7) NOT NULL,
+	[SenderId] [int] NOT NULL,
+	[ChatId] [int] NOT NULL,
+ CONSTRAINT [PK_Messages] PRIMARY KEY CLUSTERED 
 );
